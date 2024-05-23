d4447aeb89fe3a8e2cc55693211c1537240826ea
Mark Parker <marksadpepe@gmail.com>
2024-05-23 15:30:42 +0300

    refactored main app due to new services

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const fs = require("fs");
 const pp = require("puppeteer");
-const https = require("https");
 const path = require("path");
-const CatalogDto = require("./dtos/Catalog.js");
+const CatalogService = require("./services/Catalog.js");
+const FileService = require("./services/File.js");
 
 /* TODO
  * [x] - find name
@@ -12,7 +12,7 @@ const CatalogDto = require("./dtos/Catalog.js");
  * [x] - download PDF and save it to dir
  * [x] - save to JSON (file?)
  * [] - error handling
- * [] - refactoring
+ * [x] - refactoring
 */
 
 const pdfDir = process.env.PDF_DIRNAME;
@@ -25,27 +25,6 @@ if (!fs.existsSync(pdfDir)) {
 if (!fs.existsSync(jsonDir)) {
   fs.mkdirSync(jsonDir);
 }
-
-const downloadFile = (url, dst) => {
-  const dirname = path.dirname(dst);
-  if (!fs.existsSync(dirname)) {
-    throw new Error(`Specified directory does not exists ${dirname}`);
-  }
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(dst);
-    https.get(url, (res) => {
-      res.pipe(file);
-      file.on("finish", () => {
-        file.close();
-        console.log(`File successfully downloaded at: ${dst}`);
-        resolve();
-      });
-    }).on("error", (err) => {
-      fs.unlink(dst);
-      reject(err);
-    });
-  });
-};
 
 (async () => {
   const browser = await pp.launch({headless: false});
@@ -63,41 +42,22 @@ const downloadFile = (url, dst) => {
     const catalogCard = await el.$("div");
     const pdfLink = await catalogCard.$eval(".hover .zoom figcaption a:last-child", e => e.getAttribute("href"));
 
-    const catalogLinkEl = await catalogCard.$(".hover h3 a");
-    const catalogName = await catalogLinkEl.evaluate(e => e.textContent);
-    const catalogLink = await catalogLinkEl.evaluate(e => e.getAttribute("href"));
-
-    const dates = await catalogCard.$$("p time");
-    const dateStartString = await dates[0].evaluate(e => e.getAttribute("datetime"));
-    const dateEndString = await dates[1].evaluate(e => e.getAttribute("datetime"));
-
-    const pdfPath = path.resolve(pdfDir, `${catalogName}.pdf`);
-    const jsonPath = path.resolve(jsonDir, `${catalogName}.json`);
+    const catalog = await CatalogService.getCatalog(catalogCard);
+    const catalogPdfPath = path.resolve(pdfDir, `${catalog.name}.pdf`);
+    const catalogJsonPath = path.resolve(jsonDir, `${catalog.name}.json`);
 
     try {
-      await downloadFile(pdfLink, pdfPath);
+      await FileService.saveCatalogToPdf(pdfLink, catalogPdfPath);
+      console.log(`Successfully saved catalog ${catalog.name} to PDF: ${catalogPdfPath}`);
     } catch (err) {
-      console.error(`Failed to download catalog ${catalogName}: ${err.message}`);
+      console.error(`Failed to save catalog ${catalog.name} to PDF: ${err.message}`);
     }
-    const catalog = new CatalogDto(catalogName, dateStartString, dateEndString, catalogLink);
-    const data = {
-      name: catalog.name,
-      dateStart: catalog.dateStart,
-      dateEnd: catalog.dateEnd,
-      link: catalog.link
-    };
 
     try {
-      fs.writeFile(jsonPath, JSON.stringify(data), "utf-8", (err) => {
-        if (err) {
-          console.error(`Failed to save catalog ${catalogName} to JSON: ${err.message}`);
-          return;
-        }
-      });
-
-      console.log(`Catalog ${catalogName} successfully saved to ${jsonPath} JSON file`);
+      FileService.saveCatalogToJson(catalog, catalogJsonPath);
+      console.log(`Successfully saved catalog ${catalog.name} to JSON: ${catalogJsonPath}`);
     } catch (err) {
-      console.error(`Failed to save catalog ${catalogName} to JSON: ${err.message}`);
+      console.error(`Failed to save catalog ${catalog.name} to JSON: ${err.message}`);
     }
   });
 
